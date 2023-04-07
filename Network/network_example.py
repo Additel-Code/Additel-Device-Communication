eca810872687e8774c3a133db55faad4ff6e5432
MichaelEPope <michaelpopepublic@sent.com>
2023-04-07 14:17:33 -0700

    Saved changes to Network.py

--- Network/network_example.py
+++ Network/network_example.py
@@ -4,8 +4,6 @@ import socket
 # set the ip address and port
 ip_address = '192.168.7.165'
 port = 8000
-command = 'DISP:LANG?\r\n'
-# command asks the 875 to return the language the 875 is currently using (note that it ends in /r/n, that is important)
 
 # connect to the Additel device over TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,6 +11,7 @@ s.settimeout(1)
 s.connect((ip_address, port))
 
 # send the command we want to send, in bytes
+command = 'MEAS:PRES1?\r\n'
 command_in_bytes =  bytes(command, 'utf-8')
 s.send(command_in_bytes)
 
